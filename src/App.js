42bad5d46cffd4583e04f96c7c3cc583c67cc791
batyrnurhan <117359037+batyrnurkhan@users.noreply.github.com>
2023-10-17 16:52:07 +0600

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+import Profile from './components/Profile';
+import HomePage from './components/HomePage';
+import QueueSelection from './components/QueueSelection';
+import QueuesPage from './components/QueuesPage';
+import JoinQueuePage from './components/JoinQueuePage';
+import ManagerWorkplace from './components/ManagerWorkplace';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/profile" element={<Profile />} />
+                    <Route path="/home" element={<HomePage />} />
+                    <Route path="/select-queue" element={<QueueSelection />} />
+                    <Route path="/queues" element={<QueuesPage />} />
+                    <Route path="/join-queue" element={<JoinQueuePage />} />
+                    <Route path="/manager-workplace" element={<ManagerWorkplace />} />
+                    <Route path="/" exact element={<QueuesPage/>} /> 
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
