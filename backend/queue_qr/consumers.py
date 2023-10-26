006fbbf060ded9592467b2d7a4368441812b67f4
iambatyrkhan <makyzhanbatyr@gmail.com>
2023-10-26 17:07:02 +0600

    second sockets Call-Next

--- backend/queue_qr/consumers.py
+++ backend/queue_qr/consumers.py
@@ -21,3 +21,22 @@ class QueueConsumer(AsyncWebsocketConsumer):
     # Handle messages from room group
     async def send_queue_update(self, event):
         await self.send(text_data=json.dumps(event["text"]))
+
+
+
+class CallNextConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        pass
+
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        if data.get('action') == 'call_next':
+            # Здесь ваша логика для вызова следующего тикета
+            response = {
+                'type': 'ticket_called',
+                'message': 'Ticket 12 is being called in the BACHELOR queue.'
+            }
+            await self.send(text_data=json.dumps(response))
--- backend/queue_qr/routing.py
+++ backend/queue_qr/routing.py
@@ -1,7 +1,7 @@
 from django.urls import re_path
-
 from . import consumers
 
 websocket_urlpatterns = [
     re_path(r'ws/queues/$', consumers.QueueConsumer.as_asgi()),
+    re_path(r'ws/call-next/$', consumers.CallNextConsumer.as_asgi()),
 ]
